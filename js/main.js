92e265e5e8e43fe2a0645421fbb0f849e53fa293
Your NameDouglas <k.japa@outlook.com>
2025-04-01 22:22:56 -0300

    aula dia 01/04/2025

--- js/main.js
+++ js/main.js
@@ -1,22 +1,46 @@
-const initialingProducts = async() => {
+const initialingProducts = async(id) => {
     const response = await fetch('http://localhost:3000/product/list')
     const data = await response.json()
+    console.log(data)
 
     const divList = document.getElementById('product-list')
+    divList.innerHTML = ''
     data.map((product) => {
         divList.innerHTML += `
-        <div id="product-list">
         <div class="product-card">
             <div class="icon-product">
-                <span>teste</span>
+                <span>i-${product.property}</span>
             </div>
             <div class="text-products">
                 <p>${product.type}$</p>
                 <p>${product.adress}$</p>
                 <p>${product.property}$</p>
+                <button onclick="removeProduct(${product.id})">Excluir</button>
             </div>
         </div>
-    </div>
     `
     })
+}
+
+const removeProduct = async(id) => {
+    const response = await fetch(`http://localhost:3000/product/${id}`, {
+        method:'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    const data = await response.json()
+    console.log(data)
+    loadProducties()
+    return
+}
+
+const createProduct = async (event) => {
+    event.preventDefault()
+    const product = {
+        type: event.target.type.value,
+        addres: event.target.adress.value,
+        rooms: +event.target.rooms.value,
+        property: event.target.property.value,
+    }
 }
